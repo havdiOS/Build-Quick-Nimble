cbd2e52bca1ba0bc06818ee78702acaf797d9d48
Ha.vu1 <Ha.vu1@onemount.com>
2024-12-19 09:56:40 +0700

    OCD

---
+++ Carthage/Checkouts/Nimble/OCD/OCD.h
@@ -0,0 +1,19 @@
+//
+//  OCD.h
+//  OCD
+//
+//  Created by OMC-HAVU1-M on 19/12/24.
+//  Copyright © 2024 Jeff Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OCD.
+FOUNDATION_EXPORT double OCDVersionNumber;
+
+//! Project version string for OCD.
+FOUNDATION_EXPORT const unsigned char OCDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCD/PublicHeader.h>
+
+
---
+++ Carthage/Checkouts/Nimble/OCD/OCDInterface.swift
@@ -0,0 +1,13 @@
+//
+//  OCDInterface.swift
+//  OCD
+//
+//  Created by OMC-HAVU1-M on 19/12/24.
+//  Copyright © 2024 Jeff Hui. All rights reserved.
+//
+
+import Foundation
+
+public func printOCD() {
+    print("Ha.vu OCD")
+}
